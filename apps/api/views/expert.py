60be1b6f90b8be7e10eb538916917c1b33864b73
panla <pankla@163.com>
2020-08-24 10:19:55 +0800

    update 修改expert_list 接口实现

--- apps/api/views/expert.py
+++ apps/api/views/expert.py
@@ -1,23 +1,34 @@
 # -*- encoding=utf-8 -*-
 
+from django.db.models import Q
 from rest_framework import generics
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
 
 from model.models import Expert
 from api.serializers.expert import ExpertListSerializer, ExpertDetailSerializer
-from common.page import PagePagination
+from common.page import get_results
 
 
-class ExpertListView(generics.ListAPIView):
-    serializer_class = ExpertListSerializer
-    pagination_class = PagePagination
-    queryset = Expert.objects.all()
+class ExpertListView(APIView):
 
     @swagger_auto_schema(
         operation_id='expert_list', responses={200: ExpertListSerializer(many=True)}, tags=['experts']
     )
     def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+        queryset = Expert.objects.all()
+        if request.query_params.get('text'):
+            text = request.query_params.get('text')
+            queryset = queryset.filter(
+                Q(name=text) | Q(organization=text) | Q(major__contains=text) | Q(research_areas__contains=text)
+            )
+        elif request.query_params.get('keywords'):
+            keywords = request.query_params.get('keywords')
+            queryset = queryset.filter(Q(keywords__contains=keywords))
+        data = get_results(request, queryset, self, ExpertListSerializer)
+        return Response({'count': queryset.count(), 'experts': data}, status.HTTP_200_OK)
 
 
 class ExpertDetailView(generics.RetrieveAPIView):
